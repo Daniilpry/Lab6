2925d759c38976551322c12d6ab7dd6f7c749214
Daniilpry <den291110@gmail.com>
2025-05-03 12:39:29 +0300

    zd1

---
+++ zd1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *array = malloc(5 * sizeof(int));
+
+    if (array == NULL) {
+        perror("malloc failed");
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        array[i] = i * 10;
+    }
+
+    printf("Now writing out of bounds...\n");
+    array[5] = 999;  // Write overflow! Доступ до 6-го елемента, якого не існує
+
+    for (int i = 0; i <= 5; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+
+    free(array);
+    return 0;
+}
